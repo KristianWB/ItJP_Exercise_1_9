48f97a1f40b04ae623dd5945bfdc1d57cc28208f
KristianWB <42993999+KristianWB@users.noreply.github.com>
2018-11-30 10:32:00 +0100

    Initial commit

---
+++ src/AreaAndPerimeterOARectangle.java
@@ -0,0 +1,21 @@
+public class AreaAndPerimeterOARectangle {
+    public static void main(String[] args) {
+
+    /* Task will be done in two steps.
+    1) Calculate the area
+    2) Print out the result
+         */
+
+        // 1) Calculate the area
+        double area = 4.5 * 7.9; // Declaring and assigning the variable area by calculation
+
+        // 2)
+        System.out.print(
+                "The area of the rectangle is: "
+        );
+        System.out.printf(
+                "%.2f", area
+        );
+    }
+
+}
